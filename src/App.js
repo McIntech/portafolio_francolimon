e1e152da88bf1b33e780495d02f52b517e17d80a
Franco Limón <francoenriquee5@gmail.com>
2024-08-20 12:50:57 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/header';
-import Welcome from './components/welcome'
-import Journey from './components/journey'
-import Experience from './components/experience'
-import Project from './components/projects'
-import Testimonies from './components/testimonios'
-import Skills from './components/skills'
-import Correo from './components/correo'
-import Footer from './components/footer'
+import Welcome from './components/welcome';
+import Journey from './components/journey';
+import Experience from './components/experience';
+import Project from './components/projects';
+import Testimonies from './components/testimonios';
+import Skills from './components/skills';
+import Correo from './components/correo';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Welcome />
-      <Journey />
-      <Experience />
-      <Project />
-      <Testimonies />
-      <Skills />
-      <Correo />
-      <Footer />
-      {/* Otros componentes de tu aplicación */}
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/journey" component={Journey} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/projects" component={Project} />
+          <Route path="/testimonies" component={Testimonies} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/correo" component={Correo} />
+          {/* Otras rutas de tu aplicación */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
