da8467cbda00058a16ed74ae1174f96ef8f22401
Franco Limón <francoenriquee5@gmail.com>
2024-08-20 19:11:05 -0600

    update

--- src/utils/Mailer.js
+++ src/utils/Mailer.js
@@ -2,15 +2,23 @@ import axiosInstance from '../utils/AxiosConfig';
 
 const MailerService = {
   create(contactData) {
-    console.log('Intentando enviar el formulario de contacto con los siguientes datos:', contactData);
-    return axiosInstance.post('/api/v1/contacts', { contact: contactData })  // Cambiado 'post_acceptance' a 'contact'
+    console.log('Enviando el formulario de contacto con los datos:', contactData);
+
+    return axiosInstance.post('/api/v1/contacts', { contact: contactData })
       .then(response => {
         console.log('Formulario de contacto enviado con éxito:', response.data);
         return response.data;
       })
       .catch(error => {
         console.error('Error al enviar el formulario de contacto:', error.message);
-        console.error('Detalles del error:', error.response?.data || 'No additional details available');
+
+        // Proporciona más detalles si están disponibles
+        if (error.response) {
+          console.error('Detalles del error:', error.response.data);
+        } else {
+          console.error('No se pudo conectar con el servidor. Por favor, verifica tu conexión a internet.');
+        }
+
         throw error;
       });
   },
