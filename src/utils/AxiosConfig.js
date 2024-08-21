42890dafa6546e94a043e46c1b40a15358c49e89
Franco Limón <francoenriquee5@gmail.com>
2024-08-20 18:58:39 -0600

    u

--- src/utils/AxiosConfig.js
+++ src/utils/AxiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-            ? 'https://portafoliofranco-api-1803773f51c5.herokuapp.com' 
-            : 'http://localhost:3000',
+            ? 'https://myportafolio-api-e949e1d60b62.herokuapp.com' 
+            : 'http://localhost:3001',
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
 });
